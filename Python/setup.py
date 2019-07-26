8683f676dfa875e1ae2296d10e244d22a5c03907
Al Niessner <Al.Niessner@jpl.nasa.gov>
2019-07-26 11:44:07 -0700

    update setup.py
    Following the PyPI documentation, try building the package for distribution on PyPI. Will need a parent script to do more work than this.

--- Python/setup.py
+++ Python/setup.py
@@ -41,31 +41,27 @@ NTR:
 import os
 import setuptools
 
-base = 'DAWGIE-'
 dawgie = os.path.join ('dawgie', '__init__.py')
-deps = ['bokeh==0.13.0',
+deps = ['bokeh>=1.2',
         'boto3>=1.7.80',
         'GitPython>=2.1.11',
-        'gnupg>=2.3.1',
         'psycopg2>=2.7.4',
         'pydot3==1.0.9',
+        'pyxb==1.2.6',
         'requests>=2.20.0',
+        'textdistance',
         'transitions==0.6.8',
         'twisted>=18.7.0',
         ]
-version = 'ghrVersion'
-
-if version == 'ghr' + 'Version':
-    version = os.path.basename (os.path.abspath
-                                (os.path.join (os.path.dirname (__file__),
-                                               '..')))
-    version = version[len(base):] if version.startswith (base) else '0.0.0'
-    pass
-
+version = os.environ.get ('DAWGIE_VERSION', '0.0.0')
 with open (os.path.join (os.path.dirname (__file__), dawgie)) as f: t = f.read()
-t = t.replace ('${UNDEFINED}', version)
+t = t.replace ("'0.0.0'", "'{0}'".format (version))
 with open (os.path.join (os.path.dirname (__file__), dawgie), 'tw') as f:\
      f.write (t)
+with open (os.path.join (os.path.dirname (__file__), 'README.md'), 'rt') as f:\
+     description = f.read()
+with open (os.path.join (os.path.dirname (__file__), 'LICENSE'), 'rt') as f:\
+     license = f.read()
 setuptools.setup (name='dawgie',
                   version=version,
                   packages=['dawgie',
@@ -103,21 +99,12 @@ setuptools.setup (name='dawgie',
                                              'stylesheets/*.css']},
                   author='Al Niessner',
                   author_email='Al.Niessner@jpl.nasa.gov',
+                  classifiers=[ "Programming Language :: Python :: 3",
+                                "License", "Operating System :: OS Independent",
+                                ],
                   description='Data and Algorithm Work-flow Generation, Introspection, and Execution (DAWGIE) -- dynamically schedules dawgie.Tasks to be done a worker farm (cloud) and persists the dawgie.Values within the dawgie.StateVectors generated from the dawgie.Algorithms that make up the dawgie.Task.',
-                  license='''*******************************************************************************
- **
- **          Copyright 2017, by the California Institute of Technology
- **    ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
- ** Any commercial use must be negotiated with the Office of Technology Transfer
- **                  at the California Institute of Technology.
- **
- **  This software may be subject to U.S. export control laws and regulations.
- **  By accepting this document, the user agrees to comply with all applicable
- **                        U.S. export laws and regulations.
- **   User has the responsibility to obtain export licenses, or other export
- **  authority as may be required before exporting such information to foreign
- **                countries or providing access to foreign persons.
- ******************************************************************************
- ** NTR: 50482''',
+                  long_description=description,
+                  long_description_content_type="text/markdown",
+                  license=license,
                   keywords='adaptive pipeline',
-                  url='https://github.jpl.nasa.gov/niessner/SDS')
+                  url='https://github.com/niessner/DAWGIE')
