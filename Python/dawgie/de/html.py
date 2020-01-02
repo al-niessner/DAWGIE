c07dad8ea13b57b8fd703ac0eadb206d7b993a6e
John Engelke <jengelke@jpl.nasa.gov>
2020-01-02 12:09:24 -0800

    issue_75: Changed approach to keep copy of old methods guaranteeing strict backwards compatibility. Also, new method will use inherited __content value to append, then guaranteeing exact ordering with all other subclasses. Touchpoint. ... TO-DO: Modify div for ease-of-use to self-close and include explicit classes and/or style. Also, wrap __content assignments with AsIsText object to ensure they render as expected.

--- Python/dawgie/de/html.py
+++ Python/dawgie/de/html.py
@@ -37,10 +37,10 @@ NTR:
 '''
 
 import base64
-import bokeh
-import dawgie
 import html
 import io
+
+import dawgie
 
 
 class AsIsText(object):
@@ -161,6 +161,28 @@ class Visitor(Cell):
         pass
 
     def add_declaration(self, text: str, **kwds) -> None:
+        style = (' style="' + kwds['style'] + '" ') if 'style' in kwds else ''
+
+        if 'div' in kwds: self.__decl.append (kwds['div'])
+        if 'enum' in kwds:
+            if kwds['list']: self.__decl.append ('<OL' + style + '>')
+            else: self.__decl.append ('</OL>')
+            pass
+        if 'js' in kwds: self.__js.append (kwds['js'])
+        if 'list' in kwds:
+            if kwds['list']: self.__decl.append ('<UL' + style + '>')
+            else: self.__decl.append ('</UL>')
+            pass
+        if 'title' in kwds: self.__title = html.escape (text)
+
+        if text and 'title' not in kwds:
+            text = html.escape (text)
+            tag = (kwds['tag'] if 'tag' in kwds else 'p') + style
+            self.__decl.append ('<' + tag + '>' + text + '</' + tag + '>')
+            pass
+        return
+
+    def add_declaration_inline(self, text: str, **kwds) -> None:
         # attributes that apply to presentation
         # class allows application of predefined css within preloaded
         #     stylesheets files -- if file isn't preloaded there is no effect
@@ -173,12 +195,15 @@ class Visitor(Cell):
         # tags for configuration settings (e.g. __<custom_name> in class vars)
         # plain text representing a raw stylesheet that will be embedded directly
         #     into the document via a style tag
-        if 'css' in kwds: self.__css.append(kwds['css'])
+        if 'css' in kwds:
+            self.__css.append(kwds['css'])
         # fully qualified (entire) tags for js -- maintain for backwards comp
-        if 'js' in kwds: self.__js.append(kwds['js'])
+        if 'js' in kwds:
+            self.__js.append(kwds['js'])
         # list of relative uris from SV display page directory,
         #     e.g. '../../stylesheets/application.css'
-        if 'stylesheet' in kwds: self.__stylesheet.append(str(kwds['stylesheet']))
+        if 'stylesheet' in kwds:
+            self.__stylesheet.append(str(kwds['stylesheet']))
         # title composed from text, value ignored -- maintain for backwards comp
         if 'title' in kwds:
             # if defined use title value and ignore text
@@ -186,11 +211,12 @@ class Visitor(Cell):
                 self.__title = html.escape(str(kwds['title']))
             else:
                 self.__title = html.escape(text)
-
+        
         # content + tags for presentation (e.g. __decl in class vars)
         # fully qualified (entire) tags for div -- maintain for backwards comp
         #     note: closing tag must subsequently be specified, also
-        if 'div' in kwds: self.__decl.append(kwds['div'])
+        if 'div' in kwds:
+            self.__content.append(kwds['div'])
         # text content is escaped and embedded in paragraph tag by default
         #     unless different tag is specified in kwds
         if text and 'title' not in kwds:
@@ -198,7 +224,7 @@ class Visitor(Cell):
             tag = tag if len(tag.strip()) > 0 else 'p'
             tag_open = '<' + tag + idd + claz + style + '>'
             tag_close = ('</' + tag + '>') if tag not in self.void_elements else ''
-            self.__decl.append(tag_open + html.escape(text) + tag_close)
+            self.__content.append(tag_open + html.escape(text) + tag_close)
         # NOTE that list and pre are placed below text to allow a compound
         #     add_declaration statement that includes text, tag, pre and/or
         #     list that places a paragraph above the formatted content to
@@ -208,24 +234,15 @@ class Visitor(Cell):
         if 'list' in kwds:
             # value of enum is ignored but presence implies numbered list
             tag_for_list = 'ol' if 'enum' in kwds else 'ul'
-            self.__decl.append('<' + tag_for_list + idd + claz + style + '>')
+            self.__content.append('<' + tag_for_list + idd + claz + style + '>')
             for item in list(kwds['list']):
-                self.__decl.append('<li>' + html.escape(item) + '</li>')
-            self.__decl.append('</' + tag_for_list + '>')
+                self.__content.append('<li>' + html.escape(item) + '</li>')
+            self.__content.append('</' + tag_for_list + '>')
         # pre -- display preformatted text
         if 'pre' in kwds:
             tag_open = '<pre' + idd + claz + style + '>'
             tag_close = '</pre>'
-            self.__decl.append(tag_open + str(kwds['pre']) + tag_close)
-        return
-
-    # overrides method from Cell class to append to __decl buffer instead of
-    # separate buffer ... Visitor class implementations then render primitives
-    # inline with declarations.
-    def add_primitive(self, value, label: str = None) -> None:
-        content = (label + ' = ') if label else ''
-        content += str(value)
-        self.__decl.append(AsIsText('<p>' + html.escape(content) + '</p>'))
+            self.__content.append(tag_open + str(kwds['pre']) + tag_close)
         return
 
     def render(self) -> str:
@@ -235,10 +252,14 @@ class Visitor(Cell):
         buf.write(f"    <head>")
         buf.write(f"        <meta charset=\"UTF-8\">")
         buf.write(f"        <title>{self.__title}</title>")
-        buf.write(f"        <script src=\"https://cdn.pydata.org/bokeh/release/bokeh-{bokeh.__version__}.min.js\"></script>")
-        buf.write(f"        <script src=\"https://cdn.pydata.org/bokeh/release/bokeh-widgets-{bokeh.__version__}.min.js\"></script>")
-        buf.write(f"        <script src=\"https://cdn.pydata.org/bokeh/release/bokeh-tables-{bokeh.__version__}.min.js\"></script>")
-        for js in self.__js: buf.write("        " + js)
+        buf.write(f"        <script " +
+                  "src=\"https://cdn.pydata.org/bokeh/release/bokeh-{bokeh.__version__}.min.js\"></script>")
+        buf.write(f"        <script " +
+                  "src=\"https://cdn.pydata.org/bokeh/release/bokeh-widgets-{bokeh.__version__}.min.js\"></script>")
+        buf.write(f"        <script " +
+                  "src=\"https://cdn.pydata.org/bokeh/release/bokeh-tables-{bokeh.__version__}.min.js\"></script>")
+        for js in self.__js:
+            buf.write("        " + js)
         for stylesheet in self.__stylesheet:
             # <link rel = "stylesheet" type = "text/css" href = "myStyle.css" />
             # <link href="mystyles.css" rel="preload" as="style">
@@ -249,12 +270,12 @@ class Visitor(Cell):
             buf.write(tag_for_css)
         buf.write(f"    </head>")
         buf.write(f"    <body>")
-        for d in self.__decl: buf.write(d)
-        # 20200101: disabled separate queues for add_declaration and
-        #           add_primitive ... primitives are now rendered inline with
-        #           the same buffer as declaration formatting (function
-        #           add_primitive was overriden to append to __decl instead.
-        # self._render (buf)
+        # 20200102: next op for backwards compatibility
+        for d in self.__decl:
+            buf.write(d)
+        # iff using add_declaration_inline(...) then declarations are
+        #     rendered in order with add_primitive(...), tables, etc.
+        self._render(buf)
         buf.write(f"    </body>")
         buf.write(f"</html>")
         return buf.getvalue()
