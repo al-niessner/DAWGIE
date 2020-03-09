54aa28446948d0a4196a68df539b266a712280cd
Al Niessner <Al.Niessner@xxx.xxx>
2020-03-09 14:21:22 -0700

    handle invalid data exceptions
    dawgie supports two types of exceptions to be thrown: dawgie.NoValidInputDataError and dawgie.NoValidOutputDataError. In these two cases, the job should be considered a success but not move the target forward in the processing tree. This creates a try state condition which will be True (full success), None (invalid data exception), False (all other exceptions).

--- Python/dawgie/pl/worker/cluster.py
+++ Python/dawgie/pl/worker/cluster.py
@@ -86,6 +86,14 @@ def execute (address:(str,int), inc:int, ps_hint:int, rev:str):
                                         suc=True,
                                         tim=m.timing,
                                         val=nv)
+        except (NoValidInputDataError, NoValidOuputDataError) invalid:
+            logging.getLogger(__name__).exception ('Job "%s" had invalid data for run id %s and target "%s"',  str(m.jobid), str(m.runid), str(m.target))
+            m = dawgie.pl.message.make (typ=dawgie.pl.message.Type.response,
+                                        inc=m.target,
+                                        jid=m.jobid,
+                                        rid=m.runid,
+                                        suc=None,
+                                        tim=m.timing)
         except:
             logging.getLogger(__name__).exception ('Job "%s" failed to execute successfully for run id %s and target "%s"', str (m.jobid), str (m.runid), str (m.target))
             m = dawgie.pl.message.make (typ=dawgie.pl.message.Type.response,
