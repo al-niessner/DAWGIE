93057b57d889f026c4088e93562deb43470efed1
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-03 10:33:22 -0800

    saving some thoughts for another day

---
+++ Test/test_13.py
@@ -0,0 +1,70 @@
+'''
+
+COPYRIGHT:
+Copyright (c) 2015-2020, California Institute of Technology ("Caltech").
+U.S. Government sponsorship acknowledged.
+
+All rights reserved.
+
+LICENSE:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+- Neither the name of Caltech nor its operating division, the Jet
+Propulsion Laboratory, nor the names of its contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+NTR:
+'''
+
+## Save the actual work for another day, but this shows how to write one set
+## of tests in DB then test each instance by two other objects that extend it.
+
+import dawgie.context
+import unittest
+
+class DB:
+    @classmethod
+    def setup(cls):
+        return
+
+    def test_archive(self):
+        self.assertTrue (True)
+        return
+    pass
+
+class Post(DB,unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dawgie.context.db_impl = 'post'
+        DB.setup()
+        return
+    pass
+
+class Shelf(DB,unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dawgie.context.db_impl = 'shelf'
+        DB.setup()
+        return
+    pass
