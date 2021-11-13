4c04671035bf5562e14b4fa27b35093077d3c742
Al Niessner <Al.Niessner@xxx.xxx>
2021-11-13 07:25:12 -0800

    bash typo

--- .ci/util.sh
+++ .ci/util.sh
@@ -130,7 +130,7 @@ post_state ()
     echo "    -H \"Authorization: token {GHE_TOKEN}\""
     echo "     ${GHE_API_URL}/repos/${REPO}/statuses/${CIRCLE_SHA1}"
     echo "     -d \"{\"state\": \"${3}\", \"target_url\": \"${CIRCLE_BUILD_URL}\", \"description\": \"${2}\", \"context\": \"${1}\"}\" "
-}
+
     curl -XPOST \
          -H "Authorization: token ${GHE_TOKEN}" \
          ${GHE_API_URL}/repos/${REPO}/statuses/${CIRCLE_SHA1} \
