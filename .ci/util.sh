a4b3c58334630f9cac30a2354e7b7c565665cc6a
Al Niessner <Al.Niessner@xxx.xxx>
2021-11-13 07:22:13 -0800

    still have messaging problems

--- .ci/util.sh
+++ .ci/util.sh
@@ -126,10 +126,15 @@ post_state ()
         echo "$1 -- completion state: $3"
     fi
 
+    echo "curl -XPOST "
+    echo "    -H \"Authorization: token {GHE_TOKEN}\""
+    echo "     ${GHE_API_URL}/repos/${REPO}/statuses/${CIRCLE_SHA1}"
+    echo "     -d \"{\"state\": \"${3}\", \"target_url\": \"${CIRCLE_BUILD_URL}\", \"description\": \"${2}\", \"context\": \"${1}\"}\" "
+}
     curl -XPOST \
          -H "Authorization: token ${GHE_TOKEN}" \
          ${GHE_API_URL}/repos/${REPO}/statuses/${CIRCLE_SHA1} \
-         -d "{\"state\": \"${3}\", \"target_url\": \"${CIRCLE_BUILD_URL}\", \"description\": \"${2}\", \"context\": \"${1}\"}" > /dev/null 2>&1
+         -d "{\"state\": \"${3}\", \"target_url\": \"${CIRCLE_BUILD_URL}\", \"description\": \"${2}\", \"context\": \"${1}\"}"  ##> /dev/null 2>&1
 }
 
 which_port ()
