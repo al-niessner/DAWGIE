fcfb01556b1b98fd86f289bd091f26bbda5391c4
al-niessner <1130658+al-niessner@users.noreply.github.com>
2022-10-12 14:22:04 -0700

    Update post.py
    rename unused variable

--- Python/dawgie/db/post.py
+++ Python/dawgie/db/post.py
@@ -394,7 +394,7 @@ class Interface(dawgie.db.util.aspect.Container,dawgie.Dataset,dawgie.Timeline):
                              [rid, tnid, task_ID, alg_IDs, sv_IDs])
                 ids = cur.fetchall()
                 pks = [id[0] for id in ids]
-                pk_ID,alg_ID,sv_ID = ids[pks.index(max(pks))]
+                _pk_ID,alg_ID,sv_ID = ids[pks.index(max(pks))]
                 cur.execute('SELECT val_ID from Prime WHERE run_ID = %s AND ' +
                             'alg_ID = %s AND tn_ID = %s and task_ID = %s and ' +
                             'sv_ID = %s;',
