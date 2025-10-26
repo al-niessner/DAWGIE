b9a8d6792722c8d7678c75b0a513949ae0944d89
Al Niessner <Al.Niessner@xxx.xxx>
2025-10-26 11:07:26 -0700

    arhive might allow for infinite loop

--- Python/dawgie/pl/state.py
+++ Python/dawgie/pl/state.py
@@ -223,6 +223,7 @@ class FSM:
             dawgie.db.open()
 
         self.open_again = False
+        log.info(f'prior state to archive: {self.__prior}')
         getattr(self, self.__prior + '_trigger')()
         return
 
