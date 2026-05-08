8585e46513aa4eb33541ac65745730c63124d825
Al Niessner <Al.Niessner@xxx.xxx>
2026-05-08 06:38:04 -0700

    added placeholders

--- Test/nae/disk/__init__.py
+++ Test/nae/disk/__init__.py
@@ -36,3 +36,27 @@ POSSIBILITY OF SUCH DAMAGE.
 
 NTR:
 '''
+
+import dawgie
+import dawgie.base
+
+def analysis(
+    prefix: str, ps_hint: int = 0, runid: int = -1
+) -> dawgie.FactoryPlaceholder[dawgie.base.Analysis]:
+    raise NotImplementedError('placeholder until dawgie monkey patches me')
+
+
+def events() -> dawgie.FactoryPlaceholder[list[dawgie.EVENT]]:
+    raise NotImplementedError('placeholder until dawgie monkey patches me')
+
+
+def regress(
+    prefix: str, ps_hint: int = 0, target: str = '__none__'
+) -> dawgie.FactoryPlaceholder[dawgie.base.Regress]:
+    raise NotImplementedError('placeholder until dawgie monkey patches me')
+
+
+def task(
+    prefix: str, ps_hint: int = 0, runid: int = -1, target: str = '__none__'
+) -> dawgie.FactoryPlaceholder[dawgie.base.Task]:
+    raise NotImplementedError('placeholder until dawgie monkey patches me')
